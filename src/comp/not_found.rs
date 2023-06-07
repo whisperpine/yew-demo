c294e114b7d226cba857bff6be2b2a73b761d4a9
whisperpine <yusong.lai@icloud.com>
2023-06-07 23:47:53 +0800

    use sparse protocal to speed up crates indexing

--- src/comp/not_found.rs
+++ src/comp/not_found.rs
@@ -3,8 +3,8 @@ use yew::prelude::*;
 #[function_component]
 pub fn NotFound() -> Html {
     html! {
-        <div>
-            <h1 class="text-lg">{ "404 NOT FOUND" }</h1>
+        <div class="container mx-auto">
+            <h1 class="text-3xl">{ "404 NOT FOUND" }</h1>
         </div>
     }
 }
